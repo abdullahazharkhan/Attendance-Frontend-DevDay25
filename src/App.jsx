f5286601a424ef65c5f98cc2447baf2c36555b14
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2025-03-27 00:13:54 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-teal-500 text-white p-4'>
+      <h1 className='text-3xl'>Hello, Tailwind CSS!</h1>
+      <p className='text-lg'>This is a simple Tailwind CSS app.</p>
+    </div>
+  )
+}
+
+export default App
