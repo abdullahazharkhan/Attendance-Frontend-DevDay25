f613a390db80fd4c86763d1ba278b315c5d3d402
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-28 00:15:22 +0500

    Help button created ~ change text on diff page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
